ff971a8183e4340d7e9e42e617349567046bdc0f
Enbiya Göral <100806254+enbiyagoral@users.noreply.github.com>
2023-02-18 17:06:07 +0300

    Individual Photo Pages

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const Photo = require('./models/Photo');
 const app = express();
 
 // Connect DB
-moongose.connect('mongodb://127.0.0.1:27017/pcat-test-db',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+moongose.connect('mongodb://127.0.0.1:27017/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // Template Engine
@@ -16,14 +16,21 @@ app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // ROUTES
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
-  res.render('index',{
-    photos:photos
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos: photos,
+  });
+});
+app.get('/photos/:id',async(req, res) => {
+  
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo',{
+    photo:photo
   });
 });
 app.get('/about', (req, res) => {
@@ -32,10 +39,10 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
-app.post('/photos',async(req,res)=>{
-  await Photo.create(req.body)
-  res.redirect('/')
-})
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
 
 const port = 3000;
 app.listen(port, () => {
